f82e07da9431157331da11761392bfb087f7e164
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-02-15 07:51:45 -0800

    Update game.js

--- game.js
+++ game.js
@@ -3,20 +3,33 @@ var gamePattern = [];
 
 var userClickedPattern= [];
 
+var start = false;
+var level = 0;
+$(document).keypress(function(){
+    if(!start){
+
+$("h1").text("level " + level);
+        nextSequence();
+        start=true;
+    }
+})
+
  $(".btn").click(function(){
      var userChosenColour = $(this).attr("id");
 
      userClickedPattern.push(userChosenColour);
      playSound(userChosenColour)
+     animatePress(userChosenColour)
      console.log(userClickedPattern)
  })
 
 
 
 function nextSequence(){
+    level++;
+    $("h1").text("level " + level);
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
-
     gamePattern.push(randomChosenColour);
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
@@ -24,8 +37,16 @@ function nextSequence(){
 
     console.log(gamePattern);
 }
-nextSequence()
+
+
  function playSound(name) {
      var audio = new Audio("sounds/" + name + ".mp3");
    audio.play();
+ }
+
+ function animatePress(currentColour){
+    $('#'+currentColour).addClass('pressed')
+    setTimeout(function(){
+         $("#" + currentColour).removeClass("pressed");
+    },100)
  }
